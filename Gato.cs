e8e16235aadf1258484bec0f479a0eb5b83fd950
Prof. Cristiano de Paula <cristiano.paula@docente.senai.br>
2025-07-31 15:14:58 -0300

    Adicionar arquivos de projeto.

---
+++ Gato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Aula_01
+{
+    public class Gato : Animal
+    {
+        public string? Raca { get; set; }
+        public string? CorPelo { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using POO_Aula_01;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Animal cachorro = new Animal
+        {
+            Nome = "Rex",
+            Idade = 5,
+            Especie = "Cachorro"
+        };
+
+        Cachorro cachorro2 = new Cachorro
+        {
+            Nome = "Totó",
+            Idade = 2,
+            Especie = "Cachorro",
+            Raca = "Labrador",
+            TipoAlimentacao = "Ração"
+        };
+
+        Console.WriteLine($"Animal: {cachorro.Nome}, Idade: {cachorro.Idade}, Espécie: {cachorro.Especie}");
+
+        Console.WriteLine($"Cachorro: {cachorro2.Nome}, Idade: {cachorro2.Idade}, Espécie: {cachorro2.Especie}, Raça: {cachorro2.Raca}, Tipo de Alimentação: {cachorro2.TipoAlimentacao}");
+
+
+        Gato gato = new Gato
+        {
+            Nome = "Mia",
+            Idade = 3,
+            Especie = "Gato",
+            Raca = "Siamês",
+            CorPelo = "Branco"
+        };
+
+        Console.WriteLine($"Gato: {gato.Nome}, Idade: {gato.Idade}, Espécie: {gato.Especie}, Raça: {gato.Raca}, Cor do Pelo: {gato.CorPelo}");
+    }
+}
