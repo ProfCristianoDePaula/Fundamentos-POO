7c887320671cc56cc5a079287b85d2507bc8c141
Prof. Cristiano <cristiano.paula@docente.senai.br>
2025-08-08 09:04:17 -0300

    Adicionar funcionalidade para listas de frutas e alunos
    Implementada uma nova funcionalidade em `Program.cs` que permite trabalhar com listas de strings, incluindo operações de adição, alteração e remoção de itens. Também foi criada uma lista de objetos da classe `Aluno`, que armazena informações como código e nome. O arquivo `Aluno.cs` foi adicionado, definindo a classe `Aluno` com um construtor e propriedades para `Codigo` e `Nome`.

--- Aula-03/Program.cs
+++ Aula-03/Program.cs
@@ -1,7 +1,86 @@
-﻿internal class Program
+﻿using Aula_03;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("******************************* Trabalhando com Listas *******************************");
+
+        // Criando uma lista de strings
+        List<string> lista = new List<string>();
+        lista.Add("Maça"); // Adiona o item no final da lista
+        lista.Add("Banana"); // Adiona o item no final da lista
+        lista.Add("Goiaba"); // Adiona o item no final da lista
+        lista.Add("Laranja"); // Adiona o item no final da lista
+
+        Console.WriteLine("\nImprimindo os valores do 'List' na tela");
+        foreach (string item in lista) // Percorre toda a lista
+        {
+            Console.WriteLine(item); // Imprimir item por item da lista
+        }
+
+        Console.WriteLine("\nImprimir um item na posição específica");
+        Console.WriteLine($"Item na Posição 2: {lista[2]}");
+
+        // Adicionar um item em uma posição específica
+        Console.WriteLine("\nAdicionar um item na posição específica");
+        lista.Insert(1, "Tangerina");
+
+        Console.WriteLine("\nImprimindo os valores do 'List' na tela");
+        foreach (string item in lista) // Percorre toda a lista
+        {
+            Console.WriteLine(item); // Imprimir item por item da lista
+        }
+
+        // Alterando um item da lista
+        Console.WriteLine("\nAlterar um item da lista específico");
+        lista[4] = "Melancia";
+
+        foreach (string item in lista) // Percorre toda a lista
+        {
+            Console.WriteLine(item); // Imprimir item por item da lista
+        }
+
+        // Remover itens da Lista
+        Console.WriteLine("\nRemover um item da lista por posição");
+        lista.RemoveAt(3);
+        foreach (string item in lista) // Percorre toda a lista
+        {
+            Console.WriteLine(item); // Imprimir item por item da lista
+        }
+
+        Console.WriteLine("\nRemover um item da lista buscando pelo conteudo");
+        lista.Remove("Banana");
+        foreach (string item in lista) // Percorre toda a lista
+        {
+            Console.WriteLine(item); // Imprimir item por item da lista
+        }
+
+        Console.WriteLine("\nRemover todos os itens da lista");
+        lista.Clear();
+        foreach (string item in lista) // Percorre toda a lista
+        {
+            Console.WriteLine(item); // Imprimir item por item da lista
+        }
+
+
+
+        Console.WriteLine("******************************* Trabalhando com Listas de Objetos *******************************");
+
+        // Criando uma lista de Alunos
+        List<Aluno> listaAlunos = new List<Aluno>();
+
+        // Adiona o item no final da lista
+        listaAlunos.Add(new Aluno(101, "Ana"));
+        listaAlunos.Add(new Aluno(102, "Bia"));
+        listaAlunos.Add(new Aluno(103, "Carlos"));
+
+        Console.WriteLine("\nImprimindo os valores do 'List' de Alunos na tela");
+        foreach (Aluno aluno in listaAlunos) // Percorre toda a lista
+        {
+            // Imprimir item por item da lista
+            Console.WriteLine($"Código: {aluno.Codigo}, Nome: {aluno.Nome}");
+        }
+
     }
 }
